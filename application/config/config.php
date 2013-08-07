dd30209e9e1ccaa6733d306907045873c732ea9b
Brandan Majeske <bmajeske@me.com>
2013-08-07 12:40:00 -0600

    Bootstrap 3 Conversion begun

--- application/config/config.php
+++ application/config/config.php
@@ -39,7 +39,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/lucworking/';
+$config['base_url']	= 'http://localhost/luc-working-branch/';
 
 /*
 |--------------------------------------------------------------------------
