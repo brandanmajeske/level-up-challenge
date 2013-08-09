4448d6838484c11483f634d4c2bff6819e181617
brandan majeske <bmajeske@me.com>
2013-08-08 20:28:53 -0600

    user profile updates

--- application/views/home.php
+++ application/views/home.php
@@ -3,12 +3,6 @@
 <!-- Latest Projects -->
 
 <?php 
-
-echo '<h3>This is where the projects live, get you some!</h3>';
-echo '<pre>',print_r($data, true),'</pre>'; 
-?>
-
-<?php 
 if(isset($data[0]['title'])) :
 	if(count($data) < 5):
 		$j = count($data);
@@ -20,14 +14,13 @@ if(isset($data[0]['title'])) :
 		  <div class="panel-heading">
 		    <h2class="panel-title"><?php echo $data[$i]['title'];?></h2>
 		</div>
-			<p class=""><?php echo substr($data[$i]['description'], 0, 250);?> <a href="<?php echo base_url().'projects/view/'.$data[$i]['id'];?>"> <em> SEE MORE <span class="glyphicon glyphicon-arrow-right"></span></em></a></p>
+			<span class="col-10 pull-right">
+				<p class=""><?php echo substr($data[$i]['description'], 0, 250);?> <a href="<?php echo base_url().'projects/view/'.$data[$i]['id'];?>"> <em> SEE MORE <span class="glyphicon glyphicon-arrow-right"></span></em></a></p>
+			</span>
 			<img src="<?php echo base_url(). 'uploads/user_profile_img/'.$data[$i]['user_image']; ?>" class="pull-left col-2 img-thumbnail" alt="user-img"/>
 			<div class="clearfix"></div>
-			<?php echo "<p class='muted'><em>Author: ".$data[$i]['username']."<br />"."Published On: </em></p>";?>
-					
-			
-			
-		  			</div>
+			<?php echo "<p class='text-muted'><em>Author: ".$data[$i]['username']."<br />"."Published On: </em></p>";?>
+			</div>
 
 <?php 
 } 
--- application/views/projects.php
+++ application/views/projects.php
@@ -7,8 +7,8 @@
 <input class="form-control input-lg" required type="text" name="title" value="<?php echo set_value('title'); ?>"  /></label>
 </div>
 <div class="form-group">
-<label>Description<br />
-<textarea class="form-control input-lg" name="description"><?php echo set_value('description'); ?></textarea></label>
+<label>Description<br /></label>
+<textarea class="form-control input-lg" name="description"><?php echo set_value('description'); ?></textarea>
 </div>
 <input type="submit" class="btn btn-primary" value="Submit" />
 </fieldset>
